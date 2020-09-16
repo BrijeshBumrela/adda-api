2df74cb1ba55af8f8226fa47f3593591ab1e5ba7
BrijeshBumrela <brijeshbumrela@gmail.com>
2020-09-16 21:46:32 +0530

    pause & resume producer complete

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import http from 'http';
-import express from 'express';
-import socket from 'socket.io';
-import Meet from './models/Meet';
-import socketService from './services/socket';
-import bodyParser from 'body-parser';
-import { types as msTypes } from 'mediasoup';
-import msServices from './services/mediasoup';
-import apiServices from './services/api';
-import cors from 'cors';
-import config from './config';
+import http from "http";
+import express from "express";
+import socket from "socket.io";
+import Meet from "./models/Meet";
+import socketService from "./services/socket";
+import bodyParser from "body-parser";
+import { types as msTypes } from "mediasoup";
+import msServices from "./services/mediasoup";
+import apiServices from "./services/api";
+import cors from "cors";
+import config from "./config";
 
-const app = express()
+const app = express();
 const server = http.createServer(app);
 const io = socket(server);
 
@@ -24,27 +24,28 @@ let meetings: Meet[] = [];
 // List of workers used by mediasoup to create the routers(rooms)
 const workers: msTypes.Worker[] = [];
 
-
 const { createMsWorkers } = msServices();
 const { router: apiRouter } = apiServices(meetings, workers);
 
-app.use(apiRouter)
+app.use(apiRouter);
 
-io.on('connection', socket => socketService(socket, meetings, io))
+io.on("connection", socket => socketService(socket, meetings, io));
 
 const somebullshit = async () => {
-    // 4ac8423a-4f11-47e8-abfe-0dcc1e46dc09
-    await createMsWorkers(workers);
-    const meeting = new Meet("4ac8423a-4f11-47e8-abfe-0dcc1e46dc09", "my meet");
-    meetings.push(meeting);
+  // 4ac8423a-4f11-47e8-abfe-0dcc1e46dc09
+  await createMsWorkers(workers);
+  const meeting = new Meet("4ac8423a-4f11-47e8-abfe-0dcc1e46dc09", "my meet");
+  meetings.push(meeting);
 
-    const selectedWorker = workers[0]
-    const router = await selectedWorker.createRouter({ mediaCodecs: config.mediasoup.router.mediaCodecs })
-    meeting.router = router;
-}
+  const selectedWorker = workers[0];
+  const router = await selectedWorker.createRouter({
+    mediaCodecs: config.mediasoup.router.mediaCodecs
+  });
+  meeting.router = router;
+};
 
 setInterval(() => {
-    meetings = meetings.filter(meeting => meeting.friends.length > 0);
-}, 900000)
+  meetings = meetings.filter(meeting => meeting.friends.length > 0);
+}, 900000);
 
-somebullshit()
+somebullshit();
--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,15 +1,18 @@
 import { Socket, Server } from "socket.io";
 import Meet from "../models/Meet";
-import utils from '../utils/utils'
+import utils from "../utils/utils";
 import User from "../models/User";
-import msServices from '../services/mediasoup';
-import { types as msTypes } from 'mediasoup';
+import msServices from "../services/mediasoup";
+import { types as msTypes } from "mediasoup";
 
 export default async (socket: Socket, meetings: Meet[], io: Server) => {
     const { findUserAndMeeting, findMeeting } = utils(meetings);
     const { createWebRTCTransport, createConsumer } = msServices();
 
-    const { name, meetingId }: { name: string; meetingId: string } = socket.handshake.query;
+    const {
+        name,
+        meetingId
+    }: { name: string; meetingId: string } = socket.handshake.query;
 
     const user = new User(socket.id, name);
     const meeting = findMeeting(meetingId);
@@ -20,182 +23,217 @@ export default async (socket: Socket, meetings: Meet[], io: Server) => {
 
     socket.join(meeting.id);
 
-    socket.on('getRouterCapabilities', (data, callback) => {
+    socket.on("getRouterCapabilities", (data, callback) => {
         const [user, meeting] = findUserAndMeeting(socket.id);
-        if (!user) throw new Error("User not found")
-        if (!meeting) throw new Error("Meeting not found")
+        if (!user) throw new Error("User not found");
+        if (!meeting) throw new Error("Meeting not found");
         callback(meeting.router.rtpCapabilities);
-    })
+    });
 
     socket.on("createProducerTransport", async (data, callback) => {
         const [user, meeting] = findUserAndMeeting(socket.id);
-        if (!user) throw new Error("User not found")
-        if (!meeting) throw new Error("Meeting not found")
-        if (!meeting.router) throw new Error("User is not connected")
+        if (!user) throw new Error("User not found");
+        if (!meeting) throw new Error("Meeting not found");
+        if (!meeting.router) throw new Error("User is not connected");
 
-        const { params, transport } = await createWebRTCTransport(meeting.router);
+        const { params, transport } = await createWebRTCTransport(
+            meeting.router
+        );
         user.produceTransport = transport;
         callback(params);
-    })
+    });
 
     socket.on("connectProducerTransport", async (data, callback) => {
         const [user, meeting] = findUserAndMeeting(socket.id);
-        if (!user) throw new Error("User not found")
-        if (!meeting) throw new Error("Meeting not found")
-        if (!meeting.router) throw new Error("User is not connected")
+        if (!user) throw new Error("User not found");
+        if (!meeting) throw new Error("Meeting not found");
+        if (!meeting.router) throw new Error("User is not connected");
 
         const transport = user.produceTransport;
 
         try {
-            await transport.connect({ dtlsParameters: data.dtlsParameters })
-        } catch(e) {
+            await transport.connect({ dtlsParameters: data.dtlsParameters });
+        } catch (e) {
             console.error(e);
         } finally {
             callback();
         }
-    })
+    });
 
     socket.on("createConsumerTransport", async (data, callback) => {
         const [user, meeting] = findUserAndMeeting(socket.id);
-        if (!user) throw new Error("User not found")
-        if (!meeting) throw new Error("Meeting not found")
-        if (!meeting.router) throw new Error("User is not connected")
+        if (!user) throw new Error("User not found");
+        if (!meeting) throw new Error("Meeting not found");
+        if (!meeting.router) throw new Error("User is not connected");
 
-        const { params, transport } = await createWebRTCTransport(meeting.router);
+        const { params, transport } = await createWebRTCTransport(
+            meeting.router
+        );
 
         user.consumeTransport = transport;
         callback(params);
-    })
+    });
 
     socket.on("connectConsumerTransport", async (data, callback) => {
         const [user, meeting] = findUserAndMeeting(socket.id);
-        if (!user) throw new Error("User not found")
-        if (!meeting) throw new Error("Meeting not found")
-        if (!meeting.router) throw new Error("User is not connected")
+        if (!user) throw new Error("User not found");
+        if (!meeting) throw new Error("Meeting not found");
+        if (!meeting.router) throw new Error("User is not connected");
 
         const transport = user.consumeTransport;
 
         try {
-            await transport.connect({ dtlsParameters: data.dtlsParameters })
-        } catch(e) {
+            await transport.connect({ dtlsParameters: data.dtlsParameters });
+        } catch (e) {
             console.error(e);
         } finally {
             callback();
         }
-    })
+    });
 
-    socket.on('produce', async (data, callback) => {
-        const { kind, rtpParameters }: { kind: msTypes.MediaKind, rtpParameters: msTypes.RtpParameters } = data;
-        const [ user, meeting ] = findUserAndMeeting(socket.id);
+    socket.on("produce", async (data, callback) => {
+        const {
+            kind,
+            rtpParameters
+        }: {
+            kind: msTypes.MediaKind;
+            rtpParameters: msTypes.RtpParameters;
+        } = data;
+        const [user, meeting] = findUserAndMeeting(socket.id);
 
-        if (!user) throw new Error("User not found")
-        if (!meeting) throw new Error("Meeting not found")
-        if (!meeting.router) throw new Error("User is not connected")
+        if (!user) throw new Error("User not found");
+        if (!meeting) throw new Error("Meeting not found");
+        if (!meeting.router) throw new Error("User is not connected");
 
         try {
-            const producer = await user.produceTransport.produce({ kind, rtpParameters });
+            const producer = await user.produceTransport.produce({
+                kind,
+                rtpParameters
+            });
             user.producer = producer;
 
-            callback({ id: producer.id })
-            
-            socket.to(meeting.id).emit('newProducer', {
+            callback({ id: producer.id });
+
+            socket.to(meeting.id).emit("newProducer", {
                 socketId: socket.id,
                 producerId: producer.id,
                 kind
-            })
-        } catch(e) {
-            callback({ error: e.message })
+            });
+        } catch (e) {
+            callback({ error: e.message });
         }
-    })
+    });
 
-    socket.on('consume', async (data, callback) => {
-        const { rtpCapabilities, userId, transportId, kind }: {
-            rtpCapabilities: msTypes.RtpCapabilities,
-            userId: string,
-            transportId: string,
-            kind: msTypes.MediaKind
+    socket.on("consume", async (data, callback) => {
+        const {
+            rtpCapabilities,
+            userId,
+            transportId,
+            kind
+        }: {
+            rtpCapabilities: msTypes.RtpCapabilities;
+            userId: string;
+            transportId: string;
+            kind: msTypes.MediaKind;
         } = data;
 
+        const [user, meeting] = findUserAndMeeting(socket.id);
 
-        const [ user, meeting ] = findUserAndMeeting(socket.id);
-
-        if (!user) throw new Error("User not found")
-        if (!meeting) throw new Error("Meeting not found")
-        if (!meeting.router) throw new Error("User is not connected")
+        if (!user) throw new Error("User not found");
+        if (!meeting) throw new Error("Meeting not found");
+        if (!meeting.router) throw new Error("User is not connected");
 
         const producerUser = findUserAndMeeting(userId)[0];
 
-        if (!producerUser) throw new Error("Producer for this consumer does not exist")
+        if (!producerUser)
+            throw new Error("Producer for this consumer does not exist");
 
         const producer = producerUser.producer;
         const router = meeting.router;
 
-        if (!producer) throw new Error("Producer peer is not producing")
+        if (!producer) throw new Error("Producer peer is not producing");
 
-        const result = await createConsumer(producer, rtpCapabilities, user.consumeTransport, router);
+        const result = await createConsumer(
+            producer,
+            rtpCapabilities,
+            user.consumeTransport,
+            router
+        );
         if (!result) throw new Error("Some error occured");
 
         const { consumer, meta } = result;
 
-
-        consumer.on('producerpause', async () => {
+        consumer.on("producerpause", async () => {
             await consumer.pause();
-            socket.to(user.id).emit("consumerPause", { producerId: consumer.producerId })
-        })
-        
+            socket
+                .to(user.id)
+                .emit("consumerPause", { producerId: consumer.producerId });
+        });
 
-        consumer.on('producerresume', async () => {
+        consumer.on("producerresume", async () => {
             await consumer.resume();
-            socket.to(user.id).emit("consumerResume", { producerId: consumer.producerId })
-        })
+            socket
+                .to(user.id)
+                .emit("consumerResume", { producerId: consumer.producerId });
+        });
 
         user.addConsumer(consumer);
 
         callback(meta);
-    })
+    });
 
-    socket.on('pauseProducer', async ({ producerUserId }: { producerUserId: string }, callback) => {
-        const [user, meeting] = findUserAndMeeting(producerUserId);
-        if (!user || !meeting) throw new Error("User not found");
-        await user.producer.pause();
-        callback();
-    })
+    socket.on(
+        "pauseProducer",
+        async ({ producerUserId }: { producerUserId: string }, callback) => {
+            const [user, meeting] = findUserAndMeeting(producerUserId);
+            if (!user || !meeting) throw new Error("User not found");
+            await user.producer.pause();
+            callback();
+        }
+    );
 
-    socket.on('resumeProducer', async ({ producerUserId }: { producerUserId: string }, callback) => {
-        const [user, meeting] = findUserAndMeeting(producerUserId);
-        if (!user || !meeting) throw new Error("User not found");
-        await user.producer.resume();
-        callback();
-    })
+    socket.on(
+        "resumeProducer",
+        async ({ producerUserId }: { producerUserId: string }, callback) => {
+            const [user, meeting] = findUserAndMeeting(producerUserId);
+            if (!user || !meeting) throw new Error("User not found");
+            await user.producer.resume();
+            callback();
+        }
+    );
 
-    socket.on('resume', async (data, callback) => {
-        const { consumerId, socketId, kind }: { 
-            consumerId: string, 
-            socketId: string, 
-            kind: msTypes.MediaKind 
+    socket.on("resume", async (data, callback) => {
+        const {
+            consumerId,
+            socketId,
+            kind
+        }: {
+            consumerId: string;
+            socketId: string;
+            kind: msTypes.MediaKind;
         } = data;
 
-        const [ user, meeting ] = findUserAndMeeting(socket.id);
+        const [user, meeting] = findUserAndMeeting(socket.id);
 
-        if (!user) throw new Error("User not found")
-        if (!meeting) throw new Error("Meeting not found")
-        if (!meeting.router) throw new Error("User is not connected")
+        if (!user) throw new Error("User not found");
+        if (!meeting) throw new Error("Meeting not found");
+        if (!meeting.router) throw new Error("User is not connected");
 
         const producerUser = findUserAndMeeting(socketId)[0];
 
-        if (!producerUser || !producerUser.producer) throw new Error("Producer not found");
+        if (!producerUser || !producerUser.producer)
+            throw new Error("Producer not found");
 
         const consumer = user.getConsumer(consumerId);
         if (!consumer) throw new Error("Consumer object not found");
         await consumer.resume();
 
         callback();
-    })
+    });
 
-    socket.on('disconnect', () => {
+    socket.on("disconnect", () => {
         const [user, meeting] = findUserAndMeeting(socket.id);
         if (meeting instanceof Meet && user instanceof User) {
-
             const host = meeting.getHost();
 
             meeting.removeUser(socket.id);
@@ -209,94 +247,94 @@ export default async (socket: Socket, meetings: Meet[], io: Server) => {
 
             if (host.id !== newHost.id) {
                 // * Send socket stating the user about becoming the host
-                io.in(meeting.id).emit("newHost", { hostId: newHost.id })
+                io.in(meeting.id).emit("newHost", { hostId: newHost.id });
             }
-
 
             // Inform all room members about leaving of a member
             io.in(meeting.id).emit("UserLeft", { id: user.id });
         }
-    })
+    });
 
-    socket.on('getusers', (data, callback) => {
+    socket.on("getusers", (data, callback) => {
         const [user, meeting] = findUserAndMeeting(socket.id);
         if (!meeting || !user) throw new Error("Meeting not found");
         const prevUsers = meeting.friends
             .filter(friend => friend.id !== user.id)
-            .map(friend => ({ id: friend.id, name: friend.name }))
-            
+            .map(friend => ({ id: friend.id, name: friend.name }));
+
         callback(prevUsers);
-    })
+    });
 
-    socket.on('messageSend', (data: string) => {
+    socket.on("messageSend", (data: string) => {
         const [user, meeting] = findUserAndMeeting(socket.id);
         if (!meeting || !user) throw new Error("Meeting not found");
-        socket.to(meeting.id).emit('messageRecv', { message: data, user: user.name });
-    })
+        socket
+            .to(meeting.id)
+            .emit("messageRecv", { message: data, user: user.name });
+    });
 
-    socket.on('pauseSend', () => {
-        const [user, meeting] = findUserAndMeeting(socket.id);
-        if (!meeting || !user) throw new Error("Meeting not found");
-
-        if (meeting.isHost(user)) {
-            socket.to(meeting.id).emit('pauseRecv');
-        }
-    })
-
-    socket.on('playSend', () => {
+    socket.on("pauseSend", () => {
         const [user, meeting] = findUserAndMeeting(socket.id);
         if (!meeting || !user) throw new Error("Meeting not found");
 
         if (meeting.isHost(user)) {
-            socket.to(meeting.id).emit('playRecv');
+            socket.to(meeting.id).emit("pauseRecv");
         }
-    })
+    });
 
-    socket.on('seekSend', (seconds: number) => {
+    socket.on("playSend", () => {
         const [user, meeting] = findUserAndMeeting(socket.id);
         if (!meeting || !user) throw new Error("Meeting not found");
 
         if (meeting.isHost(user)) {
-            socket.to(meeting.id).emit('seekRecv', seconds);
+            socket.to(meeting.id).emit("playRecv");
         }
-    })
+    });
 
-    socket.on('nextSend', () => {
+    socket.on("seekSend", (seconds: number) => {
+        const [user, meeting] = findUserAndMeeting(socket.id);
+        if (!meeting || !user) throw new Error("Meeting not found");
+
+        if (meeting.isHost(user)) {
+            socket.to(meeting.id).emit("seekRecv", seconds);
+        }
+    });
+
+    socket.on("nextSend", () => {
         const [user, meeting] = findUserAndMeeting(socket.id);
         if (!meeting || !user) throw new Error("Meeting not found");
 
         if (meeting.isHost(user)) {
             meeting.nextSong();
-            socket.to(meeting.id).emit('nextRecv');
+            socket.to(meeting.id).emit("nextRecv");
         }
-    })
+    });
 
-    socket.on('getExistingAudioProducers', (_, cb) => {
+    socket.on("getExistingAudioProducers", (_, cb) => {
         const [user, meeting] = findUserAndMeeting(socket.id);
         if (!meeting || !user) throw new Error("Meeting not found");
 
-        const existingProducers = meeting.friends.filter(friend => friend.producer && !friend.producer.paused).map(friend => ({ id: friend.id, kind: friend.producer.kind }))
+        const existingProducers = meeting.friends
+            .filter(friend => friend.producer && !friend.producer.paused)
+            .map(friend => ({ id: friend.id, kind: friend.producer.kind }));
         cb(existingProducers);
-    })
+    });
 
-    socket.on('queueSend', (songs, cb) => {
+    socket.on("queueSend", (songs, cb) => {
         const [user, meeting] = findUserAndMeeting(socket.id);
         if (!meeting || !user) throw new Error("Meeting not found");
 
         meeting.songs = songs;
 
-        socket.to(meeting.id).emit('queueRecv', songs)
-    })
+        socket.to(meeting.id).emit("queueRecv", songs);
+    });
 
-    socket.on('queueRecv', (_, cb) => {
+    socket.on("queueRecv", (_, cb) => {
         const [user, meeting] = findUserAndMeeting(socket.id);
         if (!meeting || !user) throw new Error("Meeting not found");
         cb(meeting.songs);
-    })
+    });
 
     // Inform all room members about the addition of new member
     io.in(meeting.id).emit("UserAdded", { name: user.name, id: user.id });
-}
-
-
-
+};
